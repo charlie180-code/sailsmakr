fcb124fcd48d8d19328ead22ef5e1e2b3aca6dbd
expo180 <badersaadousalissou@gmail.com>
2025-01-15 17:15:38 +0100

    PRODUCTION_DATABASE_URL setup

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{get_database_path('app.db')}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URL')
     PLACEHOLDER_STATIC_URL = os.environ.get('PLACEHOLDER_PRODUCTION_STATIC_URL')
     LOGIN_URL = os.environ.get('PRODUCTION_LOGIN_URL')
     SERVER_URL = os.environ.get('PRODUCTION_SERVER_URL')
