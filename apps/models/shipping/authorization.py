e7a3aa5ad7ea53e529def0e4e54ddc641bdf78ff
expo180 <badersaadousalissou@gmail.com>
2025-03-10 09:42:50 +0100

    Authorization model revisited

--- apps/models/shipping/authorization.py
+++ apps/models/shipping/authorization.py
@@ -7,9 +7,9 @@ class Authorization(db.Model):
     client_first_name = db.Column(db.String, nullable=False)
     client_last_name = db.Column(db.String, nullable=False)
     client_phone_number = db.Column(db.String, nullable=False)
-    client_email_address = db.Column(db.String, unique=True)
+    client_email_address = db.Column(db.String)
     client_id_card_url = db.Column(db.String)
-    client_signature_url = db.Column(db.String, nullable=False)
+    client_signature_url = db.Column(db.String)
     client_location = db.Column(db.String, nullable=False)
     agent_first_name = db.Column(db.String, nullable=False)
     agent_last_name = db.Column(db.String, nullable=False)
@@ -33,29 +33,3 @@ class Authorization(db.Model):
 
     # ForeignKey to link to Company
     company_id = db.Column(db.Integer, db.ForeignKey('companies.id'), nullable=False)
-
-    def __init__(self, client_first_name, client_last_name, client_phone_number, client_email_address, client_id_card_url, client_signature_url, client_location, agent_first_name, agent_last_name, agent_email_address, shipping_company_title, provider_email1=None, provider_email2=None, provider_email3=None, provider_email4=None, provider_name1=None, provider_name2=None, provider_name3=None, provider_name4=None, lading_bills_identifier=None, service_fees=0.0, granted=False, user_id=None, company_id=None):
-        self.client_first_name = client_first_name
-        self.client_last_name = client_last_name
-        self.client_phone_number = client_phone_number
-        self.client_email_address = client_email_address
-        self.client_id_card_url = client_id_card_url
-        self.client_signature_url = client_signature_url
-        self.client_location = client_location
-        self.agent_first_name = agent_first_name
-        self.agent_last_name = agent_last_name
-        self.agent_email_address = agent_email_address
-        self.shipping_company_title = shipping_company_title
-        self.provider_email1 = provider_email1
-        self.provider_email2 = provider_email2
-        self.provider_email3 = provider_email3
-        self.provider_email4 = provider_email4
-        self.provider_name1 = provider_name1
-        self.provider_name2 = provider_name2
-        self.provider_name3 = provider_name3
-        self.provider_name4 = provider_name4
-        self.lading_bills_identifier = lading_bills_identifier
-        self.service_fees = service_fees
-        self.granted = granted
-        self.user_id = user_id
-        self.company_id = company_id
