31ab4d1a4da2ff18b2187e301ea563a16f9a32ca
charlie180-code <badersaadousalissou@gmail.com>
2025-04-17 13:26:38 +0100

    Revert "archive category model"
    This reverts commit 6daf5aeda053f91ea0ffb7acbb93474c236d4796.

--- apps/models/banking/archive_category.py
+++
@@ -1,8 +0,0 @@
-from ... import db
-
-class ArchiveCategory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-
-    archives = db.relationship('Archive', backref='category_obj')
